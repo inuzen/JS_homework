9f8e9b015327774b56e567aac48bdd3e060eb278
inuzen <condddr@gmail.com>
2019-06-05 18:01:58 +0300

    added task 4

--- 9/js/script.js
+++ 9/js/script.js
@@ -37,3 +37,53 @@ for (let i = -4.99; i < 5; i += 0.01) {
 let data = [line];
 
 Plotly.newPlot('plot', data);
+
+
+console.log("Task 4");
+// Написать функцию генерации поля n x n (значение n - аргумент функции), в ячейки рандомно спрятать несколько призов.
+// Пользователю дается 3 попытки найти их - по нажатию на ячейку либо открывается приз (иконка, изменения цвета и тд), либо иконка, сообщающая, что приза нет.
+// Количество оставшихся попыток выводим рядом с игровым полем.
+let prize = {
+  book: "Книга",
+  car: "Машина!",
+  horse: "Конь!",
+  empty: "Пусто!",
+  empty2: "Пусто!",
+  empty3: "Пусто!",
+  getPrize: function() {
+    let rng = Math.floor(Math.random() * (Object.keys(this).length - 2));
+    return this[Object.keys(this)[rng]];
+  }
+};
+
+let count = 3;
+
+function createBox(container) {
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.style.backgroundColor = "yellow";
+  let counter = document.querySelector(".counter");
+  card.onclick = function() {
+    if (count > 0) {
+      this.textContent = prize.getPrize();
+      this.style.backgroundColor = "whitesmoke";
+      count--;
+      counter.textContent = count;
+    }
+  }
+  container.appendChild(card);
+}
+
+function create_field(n) {
+  let box = document.querySelector(".field");
+  for (let i = 0; i < n; i++) {
+    let container = document.createElement("div");
+    container.classList.add("flex-container");
+    for (let j = 0; j < n; j++) {
+      createBox(container);
+    }
+    box.appendChild(container);
+  }
+}
+
+create_field(4);
