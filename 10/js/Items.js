417df03cf3076480ca42fa503829019b8a416d82
inuzen <condddr@gmail.com>
2019-06-09 15:25:37 +0300

    added HW 10

---
+++ 10/js/Items.js
@@ -0,0 +1,51 @@
+console.log("Task 1");
+function Item (name,price){
+  this.name = ""
+  this.price = 0;
+
+  this.create=function(name,price){
+    if (name && !price.isNaN&& price>=0) {
+      this.name = name;
+      this.price = price;
+    }
+};
+
+}
+
+function ItemBag () {
+  let items = [];
+  let itemCount = 0;
+
+  let itemSum = 0;
+
+  this.add = function(...item){
+    item.forEach((it)=>{
+      if(it instanceof Item){
+      items.push(it);
+      itemCount++;
+      itemSum += it.price;
+    }
+    });
+  };
+  this.getCount = function () {
+    return itemCount;
+  }
+  this.getCountL = function () {
+    return itemCountL;
+  }
+  this.getSum = function () {
+    return itemSum;
+  }
+}
+
+let milk = new Item();
+milk.create("milk", 10);
+let bread = new Item();
+bread.create("bread", 5);
+let eggs = new Item();
+eggs.create("eggs", 20);
+
+let bag = new ItemBag();
+bag.add(milk,bread,eggs);
+console.log(bag.getCount());
+console.log(bag.getSum());
